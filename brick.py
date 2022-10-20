8761fbc11affee37690ee10015b67409cfce549a
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2022-10-19 23:08:10 -0400

    Update brick.py
    add code for the paddle the player uses

--- brick.py
+++ brick.py
@@ -13,3 +13,20 @@ PADDLE_HEIGHT = 15
 BALL_RADIUS = 10
 
 LIVES_FONT = pygame.font.SysFont("comicsans", 4)
+
+class Paddle:
+    VEL = 5
+
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def draw(self, win):
+        pygame.draw.rect(
+            win, self.color, (self.x, self.y, self.width, self.height))
+
+    def move(self, direction=1):
+        self.x = self.x + self.VEL * direction
