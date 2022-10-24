deaeeaace02f3d8dafe73703a1a9cea84df7f9aa
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2022-10-24 17:22:29 -0400

    Update brick.py
    Added the velocity of the ball to move in the screen properly.

--- brick.py
+++ brick.py
@@ -42,4 +42,14 @@ class Ball:
         self.x_vel = 0
         self.y_vel = -self.VEL
         
-   
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+    def set_vel(self, x_vel, y_vel):
+        self.x_vel = x_vel
+        self.y_vel = y_vel
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
