600bed884e6aab495edae833de39d2b2f4dea7f6
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2022-10-24 17:24:17 -0400

    Update brick.py
    Added properties of collision between ball and bricks

--- brick.py
+++ brick.py
@@ -69,3 +69,24 @@ class Ball:
             win, self.color, (self.x, self.y, self.width, self.height))
 
 
+    def collide(self, ball):
+        if not (ball.x <= self.x + self.width and ball.x >= self.x):
+            return False
+        if not (ball.y - ball.radius <= self.y + self.height):
+            return False
+
+        self.hit()
+        ball.set_vel(ball.x_vel, ball.y_vel * -1)
+        return True
+
+    def hit(self):
+        self.health -= 1
+        self.color = self.interpolate(
+            *self.colors, self.health/self.max_health)
+
+    @staticmethod
+    def interpolate(color_a, color_b, t):
+        # 'color_a' and 'color_b' are RGB tuples
+        # 't' is a value between 0.0 and 1.0
+        # this is a naive interpolation
+        return tuple(int(a + (b - a) * t) for a, b in zip(color_a, color_b))
